e0d39c43c81c21d50c250a9d833d9d012ff7a55b
CNMathon <cnmathon@gmail.com>
2020-09-18 20:36:58 +0800

    perf(插件): 插件更新为自动化加载

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import { AppConfig } from './config/app'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
+import { AppConfig } from '@/config/app'
 
-import { loadAtdComponent } from '@/plugins/antd'
-import { registeGlobalComponent } from './components/index'
+import { loadAllPlugins } from '@/plugins'
+import { registeGlobalComponent } from '@/components/index'
 
 /** 导入第三方插件 */
 import '@/plugins'
@@ -14,8 +14,8 @@ import '@/plugins'
 const app: ReturnType<typeof createApp> = createApp(App)
 app.config.globalProperties = AppConfig
 
-/** 手动注册 antd-vue 组件 */
-loadAtdComponent(app)
+/** 加载所有 Plugins */
+loadAllPlugins(app)
 
 /** 自动注册全局组件  */
 registeGlobalComponent(app)
--- src/plugins/antd.ts
+++ src/plugins/antd.ts
@@ -4,10 +4,10 @@ import { createApp } from 'vue'
 /**
  * @description 手动注册 antd-vue 组件,达到按需加载目的
  * @description Automatically register components under Button, such as Button.Group
- * @param {ReturnType<typeof createApp>} app  整个应用的实例
+ * @param {ReturnType<typeof createApp>} app 整个应用的实例
  * @returns void
  */
-export function loadAtdComponent(app: ReturnType<typeof createApp>) {
+export default function loadComponent(app: ReturnType<typeof createApp>) {
   app.use(Button)
   app.use(Card)
   app.use(Row)
