e072b49bd6b3f707e88a8fbb8035667637d5816d
白唯 <997132391@qq.com>
2020-12-02 12:04:18 +0800

    fix(eslint): 修复 lint 错误

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,41 @@
 module.exports = {
-	root: true,
-	parserOptions: {
-		parser: '@typescript-eslint/parser'
-	},
-	env: {
-		browser: true,
-		es6: true,
-		node: true
-	},
-	plugins: ['prettier', 'vue', '@typescript-eslint/eslint-plugin'],
-	rules: {
-		'prettier/prettier': 'error',
-		'no-unused-vars': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/member-delimiter-style': 'off',
-		'@typescript-eslint/no-var-requires': 'off',
-		'@typescript-eslint/ban-ts-ignore': 'off'
-	},
-	extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript/recommended'],
-	overrides: [
-		{
-			files: ['**/tests/*.{j,t}s?(x)', '**/tests/**/*.spec.{j,t}s?(x)', '**/tests/*.spec.{j,t}s?(x)'],
-			env: {
-				mocha: true
-			}
-		}
-	]
+  root: true,
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  plugins: ['prettier', 'vue', '@typescript-eslint/eslint-plugin'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/class-name-casing': 'off',
+    'vue/valid-v-slot': 'off',
+    'no-debugger': 'off',
+    'vue/experimental-script-setup-vars': 'off'
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended'
+  ],
+  overrides: [
+    {
+      files: [
+        '**/tests/*.{j,t}s?(x)',
+        '**/tests/**/*.spec.{j,t}s?(x)',
+        '**/tests/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        mocha: true
+      }
+    }
+  ]
 }
